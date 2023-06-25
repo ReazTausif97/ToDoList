056e97036a50fc0e5dbab6f3649271abc2d42396
ReazTausi97 <reaztausifff@gmail.com>
2023-06-25 18:24:30 +0600

    first

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+
+function App() {
+  return (
+    <>
+      <section className="w-[380px] bg-[#FFFDF4] rounded-3xl">
+        <h2>asdkja</h2>
+        <h2>asdkja</h2>
+        <h2>asdkja</h2>
+        <h2>asdkja</h2>
+      </section>
+    </>
+  );
+}
+
+export default App;
