46f7bff31649628ff7c285fe4e0a52f80f4bbf12
ReazTausi97 <reaztausifff@gmail.com>
2023-06-25 20:21:56 +0600

    task css added

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Task from "./Task";
 function App() {
   return (
     <>
-      <section className="w-[380px] bg-[#000000] text-white rounded-3xl px-10 py-5">
+      <section className="w-[380px] bg-[#000000] text-white rounded-3xl px-5 py-10">
         <h2 className="font-bold text-xl">Today's Task</h2>
         <Task />
         <Task />
